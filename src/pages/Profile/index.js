60ae45849c0282101c3511d8b344a4592c14d5a7
rafaelgcaldas <rafaelzinhogomes22@live.com>
2020-03-31 22:56:59 -0300

    Create new incident and logout

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiPower, FiTrash2 } from 'react-icons/fi';
 
 import api from '../../services/api';
@@ -10,6 +10,8 @@ import logoImg from '../../assets/logo.svg';
 export default function Profile() {
 
     const [incidents, setIncidents] = useState([]);
+
+    const history = useHistory();
 
     const ongName = localStorage.getItem('ongName');
     const ongId = localStorage.getItem('ongId');
@@ -37,6 +39,11 @@ export default function Profile() {
         }
     }
 
+    async function handleLogout() {
+        localStorage.clear();
+        history.push('/');
+    }
+
     return(
         <div className="profile-container">
             <header>
@@ -44,7 +51,7 @@ export default function Profile() {
                 <span>Bem vinda, { ongName }</span>
 
                 <Link to="/incidents/new" className="button"> Cadastrar novo caso</Link>
-                <button type="button">
+                <button type="button" onClick={handleLogout}>
                     <FiPower size={18} color="E02041" />
                 </button>
             </header>
